7db2ced29a0ca52baca225508a437a41999ad8d4
Joel Barkley <tubulartreehouse@gmail.com>
2024-04-22 13:09:20 -0600

    Adds hovering to the hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,14 @@ export default function Home() {
   return (
     <div className="mx-4 md:mx-16">
       <div className="flex flex-col md:flex-row items-center">
-        <Image 
-          src={'https://res.cloudinary.com/dcvehjjfy/image/upload/v1713804415/me_qp2tst.png'}
-          width={350}
-          height={300}
-          alt="hero-picture"
-        />
+        <div className="hovering-image">
+          <Image 
+            src={'https://res.cloudinary.com/dcvehjjfy/image/upload/v1713804415/me_qp2tst.png'}
+            width={350}
+            height={300}
+            alt="hero-picture"
+          />
+        </div>
         <span className="text-textColor text-center text-4xl md:text-5xl mt-4 md:mt-0 md:ml-4">
           Hi, I’m Joel, and this is the Tubular Treehouse. Welcome in!
         </span>
